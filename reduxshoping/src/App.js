18ce4e0f285c68a7e41f3f54dbfaf116cd158771
deepanshu <deepanshu@tothenew.com>
2019-04-12 11:40:49 +0530

    commit done

--- reduxshoping/src/App.js
+++ reduxshoping/src/App.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import {addItem, decreaseItem, removeItem} from "./actions/user.actions";
 import {connect} from "react-redux";
+import Cart from "./components/cart";
+import ProductList from "./components/productList";
+
 
 class App extends Component {
+  addItem = (id) => {
+    this.props.addItem(id);
+  };
+
+  removeItem = (id, quantity) => {
+    this.props.removeItem(id, quantity);
+  };
+
+  decreaseItem = (id) => {
+    this.props.decreaseItem(id);
+  };
+
   render() {
     return (
-        <div className="App">
-        </div>
+      <div className="App">
+        <ProductList/>
+        <Cart/>
+      </div>
     );
   }
 }
-export default App;
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+// const mapDispatchToProps = (dispatch) => (
+//     {
+//       addItem: (id) => dispatch(addItem(id))
+//     }
+// )
+
+const mapDispatchToProps = {
+  addItem, removeItem, decreaseItem
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
